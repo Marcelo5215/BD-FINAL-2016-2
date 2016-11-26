80c88aadb7dd24184a2a6ffa755d0dd49d6362aa
Axelmar <marcelo.acn@hotmail.com>
2016-11-26 17:45:06 -0200

    Add files via upload
    Trigger e stored procedure

---
+++ Trigger.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE bk_pessoa (
+  cpf integer NOT NULL,
+  nome character varying(50),
+  orgid_org_superior integer
+);
+
+
+-- Stored Procedure
+CREATE OR REPLACE FUNCTION salvaexcluido()
+RETURNS trigger AS
+$BODY$ BEGIN
+INSERT INTO bk_pessoa VALUES (old.cpf, old.nome, old.orgid_org_superior);
+
+RETURN NULL;
+
+END; $BODY$
+LANGUAGE 'plpgsql';
+
+CREATE TRIGGER excluir_pessoa
+AFTER DELETE
+ON pessoa
+FOR EACH ROW
+EXECUTE PROCEDURE salvaexcluido();
