13cfd221981fb02bcb366b2b1040e36605b5ebb2
Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>
2016-11-26 20:24:44 -0200

    Renomear .csv para facilitar
    Signed-off-by: Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>

---
+++ inserir_dados.sql
@@ -0,0 +1,5 @@
+copy public.FUNCAO from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/FUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.ORG_SUPERIOR from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/ORG_SUPERIOR.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.PESSOA from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/PESSOA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.ACAO from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/ACAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.DOCPAG from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/DOCPAG.csv' with (FORMAT csv, DELIMITER E'\t');
