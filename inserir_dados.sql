b0ede3652ec1bd07cdc55e8310821d22a19616fd
Marcelo5215 <marcelo.araujo1296@gmail.com>
2016-11-27 01:00:23 -0200

    InserirDados

--- inserir_dados.sql
+++ inserir_dados.sql
@@ -1,5 +1,12 @@
-copy public.FUNCAO from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/FUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.ORG_SUPERIOR from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/ORG_SUPERIOR.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.PESSOA from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/PESSOA.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.ACAO from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/ACAO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.DOCPAG from '/home/rafael/BD-FINAL-2016-2/Diarias/D-1507/DOCPAG.csv' with (FORMAT csv, DELIMITER E'\t');
+﻿copy public.org_superior from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ORG_SUPERIOR.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.org_subordinado from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ORG_SUBORDINADO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.pessoa from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\PESSOA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.funcao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\FUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.unidade_gestora from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\UNIDADE_GESTORA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.subfuncao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\SUBFUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.programa from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\PROGRAMA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.acao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ACAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.docpag from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\DOCPAG.csv' with (FORMAT csv, DELIMITER E'\t');
+
+
+
