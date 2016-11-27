2b96c293de6a56cb40339561d095af2098624904
Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>
2016-11-27 09:16:40 -0200

    Adicona querys
    Signed-off-by: Rafael Lourenço de Lima Chehab <chehabrafael@gmail.com>

--- inserir_dados.sql
+++ inserir_dados.sql
@@ -1,12 +1,9 @@
-﻿copy public.org_superior from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ORG_SUPERIOR.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.org_subordinado from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ORG_SUBORDINADO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.pessoa from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\PESSOA.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.funcao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\FUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.unidade_gestora from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\UNIDADE_GESTORA.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.subfuncao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\SUBFUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.programa from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\PROGRAMA.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.acao from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\ACAO.csv' with (FORMAT csv, DELIMITER E'\t');
-copy public.docpag from 'D:\Documentos\BD\BD-FINAL-2016-2\Diarias\D-1507\DOCPAG.csv' with (FORMAT csv, DELIMITER E'\t');
-
-
-
+﻿copy public.org_superior from 'path_to_directory/ORG_SUPERIOR.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.org_subordinado from 'path_to_directory/ORG_SUBORDINADO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.pessoa from 'path_to_directory/PESSOA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.funcao from 'path_to_directory/FUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.unidade_gestora from 'path_to_directory/UNIDADE_GESTORA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.subfuncao from 'path_to_directory/SUBFUNCAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.programa from 'path_to_directory/PROGRAMA.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.acao from 'path_to_directory/ACAO.csv' with (FORMAT csv, DELIMITER E'\t');
+copy public.docpag from 'path_to_directory/DOCPAG.csv' with (FORMAT csv, DELIMITER E'\t');
